28e10daf04796fc8d81b07c8616e996e14a3067e
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2023-12-21 00:15:12 +0530

    day 1

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Deposit some money
+// 2. Determine number of of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if user won
+// 6. give the user their winnings.
+// 7. play agian
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
