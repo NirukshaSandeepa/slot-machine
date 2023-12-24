f6472392d61a491599de206511702a0fffed7676
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2023-12-24 11:48:42 +0530

    6. give the user their winnings.

--- project.js
+++ project.js
@@ -111,7 +111,7 @@ const transpose = (reels) => {
 // output the rows
 const printRows = (rows) => {
     for (const row of rows) {
-        let rowString = "A";
+        let rowString = "";
         for (const[i,symbol] of rows.entries()){
             rowString += symbol;
             if(i != row.length - 1){
@@ -122,7 +122,28 @@ const printRows = (rows) => {
     }
 }
 
+// 5. Check if user won
+// 6. give the user their winnings.
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
 
+    for (let row = 0; row<lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols){
+            if (symbol != symbols[0]){
+                allSame = false;
+                break;
+            }
+        }
+        
+        if (allSame){
+            winnings += bet * SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
@@ -130,3 +151,5 @@ const bet = getBet(balance,numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
 printRows(rows);
+const winnings = getWinnings(rows, bet, numberOfLines)
+console.log("You won, $" + winnings.toString())
